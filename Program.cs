91d5121a8810b2b85cb4fb4be7307f7eada31aea
MilanVulin <vulinmb@gmail.com>
2022-04-06 13:26:01 +0200

    Pop vs Peek functinonality showcased

--- Program.cs
+++ Program.cs
@@ -34,6 +34,36 @@ namespace Stack
             }
             Console.WriteLine("\n");
 
+            // Now when we know how the stack stores its numbers lets test out some of its methods
+            // Peek vs Pop 
+
+            // Peek - only returns the value, doesn't remove it from the stack 
+            Console.WriteLine("Peeking the last element ");
+            Console.WriteLine(myStack.Peek());
+            Console.WriteLine("Stack after peek: ");
+            foreach (int num in myStack)
+            {
+                Console.Write(num + " ");
+            }
+            Console.WriteLine("\n");
+
+            // Pop - returns and removes the value from the stack
+            Console.WriteLine("Poping the last element ");
+            Console.WriteLine(myStack.Pop());
+            Console.WriteLine("Stack after pop: ");
+            foreach (int num in myStack)
+            {
+                Console.Write(num + " ");
+            }
+            Console.WriteLine("\n");
+
+            // Now lets see if the stack contains user input number
+            Console.WriteLine("Type in the number that you want to find in stack!");
+            Console.Write("Number to find: ");
+            int userNumber = Int32.Parse(Console.ReadLine());
+
+            // Now that we have the number we' ll use Contains() method to see if it's in the stack
+            Console.WriteLine("The stack" + (myStack.Contains(userNumber) ? " contains " : " does not contain ") + "the number {0}", userNumber);
 
         }
     }
