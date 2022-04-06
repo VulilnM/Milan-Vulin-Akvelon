7571e722906f023f9f92a5eaaa53a843664b5bf2
MilanVulin <vulinmb@gmail.com>
2022-04-06 17:30:46 +0200

    Added and linked necessary classes

---
+++ Animal.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace OOP_Principles_Implementation
+{
+    abstract class Animal
+    {
+    }
+}
---
+++ Chicken.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace OOP_Principles_Implementation
+{
+    class Chicken : Animal
+    {
+    }
+}
