afc3cd6d0bd38f8f8b106504104e17299e465a9a
MilanVulin <vulinmb@gmail.com>
2022-04-06 18:05:21 +0200

    Implemented the Cat class

--- Cat.cs
+++ Cat.cs
@@ -6,5 +6,28 @@ namespace OOP_Principles_Implementation
 {
     class Cat : Animal
     {
+        int lengthOfClaws { get; set; }
+
+        public Cat(int lengthOfClaws, string AnimalType, string AnimalSubType, int NumberOfLegs, string TypeOfFur)
+            : base(AnimalType, AnimalSubType, NumberOfLegs, TypeOfFur)
+        {    
+            this.lengthOfClaws = lengthOfClaws;
+        }
+
+        protected override string makeSound()
+        {
+            return "MEOW!";
+        }
+
+        public override void presentSelf()
+        {
+            Console.WriteLine("Animal type: " + animalType + "\n" +
+                              "Animal subtype: " + animalSubType + "\n" +
+                              "Number of legs: " + numberOfLegs + "\n" +
+                              "Type of Fur: " + typeOfFur + "\n" +
+                              "Lenfth of claws: " + lengthOfClaws + "\n" +
+                              "Sound: " + makeSound()
+                               );
+        }
     }
 }
