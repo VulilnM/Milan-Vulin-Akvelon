2b2c89ad9298950bd331a60184f498f52339a318
MilanVulin <vulinmb@gmail.com>
2022-04-06 14:52:59 +0200

    Added reference type passing

--- Program.cs
+++ Program.cs
@@ -2,12 +2,22 @@
 
 namespace Ref_vs_Vall_Passing
 {
+    public class Person {
+        internal string FullName { get; set; }
+    }
+
     class Program
     {
         private static void ChangeValue(int val)
         {
             val = val + 1;
             Console.WriteLine("Value while in the method: " + val);
+        }
+
+        private static void ChangeReference(Person pers)
+        {
+            pers.FullName = "Petar Petrovic";
+            Console.WriteLine("Value while in the method: " + pers.FullName);
         }
 
         static void Main(string[] args)
@@ -21,6 +31,18 @@ namespace Ref_vs_Vall_Passing
             ChangeValue(value);
 
             Console.Write("Value after the changing method: {0}\n\n", value);
+
+
+            // Reference Type
+            // =====================================================================
+            Person person1 = new Person();
+            person1.FullName = "Milan Vulin";
+
+            Console.Write("My mane before the changing method: {0}\n", person1.FullName);
+
+            ChangeReference(person1);
+
+            Console.Write("My name after the changing method: {0}\n", person1.FullName);
 
         }
     }
