58914511ee6648316d042b6a7961e41e3aa2fab4
MilanVulin <vulinmb@gmail.com>
2022-04-06 13:58:47 +0200

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Ref_vs_Vall_Passing
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
