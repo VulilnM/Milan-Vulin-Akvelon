0efcba21fe9311260b34296ea4e64b28e3530088
MilanVulin <vulinmb@gmail.com>
2022-04-06 13:08:34 +0200

    Filled the stack with numbers from array and displayed both the stack and arr to see how the numbers are stored

--- Program.cs
+++ Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace Stack
 {
@@ -6,7 +7,34 @@ namespace Stack
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            // Declare an empty stack
+            Stack<int> myStack = new Stack<int>();
+
+            
+            // First we'll make an array with some numbers
+            int[] arr = { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 };
+
+            //Now we'll print out the arr
+            Console.WriteLine("Array members: ");
+            foreach (int num in arr)
+            {
+                Console.Write(num + " ");
+            }
+            Console.WriteLine("\n");
+
+            // Fill the stack with values
+            foreach (int num in arr) 
+                myStack.Push(num);
+
+            // Let's see the result
+            Console.WriteLine("Stack members: ");
+            foreach (int num in myStack)
+            {
+                Console.Write(num + " ");
+            }
+            Console.WriteLine("\n");
+
+
         }
     }
 }
