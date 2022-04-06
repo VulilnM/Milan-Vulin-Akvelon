e9c37f1098f17b125d8261bf4d80657e9018b2a6
MilanVulin <vulinmb@gmail.com>
2022-04-06 11:08:45 +0200

    Initialised a List with numbers 1 through 10 and printed it out

--- Program.cs
+++ Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace List
 {
@@ -6,7 +7,18 @@ namespace List
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            // Initialize a list with numbers 1 through 10 
+            List<int> numbers = new List<int>() { 1,2,3,4,5,6,7,8,9,10 };
+
+            //Print the list
+            Console.WriteLine("List contains following numbers:");
+            foreach (int num in numbers) {
+                //If it is the last element of the list - dont print the comma
+                if (numbers[numbers.Count - 1] != num)
+                    Console.Write(num + ", ");
+                else
+                    Console.Write(num);
+            }
         }
     }
 }
