1c613ab1a564a6c8686ba88e1667906292942a06
MilanVulin <vulinmb@gmail.com>
2022-04-14 16:52:42 +0200

    Implemented Context class for middleware communication

---
+++ Models/AppDbContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace Akvelon_Internship_Test_Task.Models
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
+        {
+        }
+
+        DbSet<Task> Task { get; set; }
+        DbSet<Project> Project { get; set; }
+
+    }
+}
