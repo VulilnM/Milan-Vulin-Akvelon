9580b746d78f54e0379ed88f687cb8b3bac626e5
MilanVulin <vulinmb@gmail.com>
2022-04-14 16:37:47 +0200

    Implemented Project class (Model)

---
+++ Models/Project.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Akvelon_Internship_Test_Task.Models
+{
+    public class Project
+    {
+
+        [Key]
+        public int ProjectId { get; set; }
+
+        [Required]
+        public string ProjectName { get; set; }
+
+        [Required]
+        [Column(TypeName = "date")]
+        public DateTime StartDate { get; set; }
+
+
+        [Column(TypeName = "date")]
+        public DateTime? CompletionDate { get; set; }
+
+        public enum ProjectStatus
+        {
+            NotStarted,
+            Active,
+            Completed
+        }
+    }
+}
