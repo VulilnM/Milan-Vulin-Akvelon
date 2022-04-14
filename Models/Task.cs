3644f921e7587017d0f3ccbcbe660a54387edefb
MilanVulin <vulinmb@gmail.com>
2022-04-14 16:40:37 +0200

    Implemented Task class (Model)

---
+++ Models/Task.cs
@@ -0,0 +1,30 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Akvelon_Internship_Test_Task.Models
+{
+    public class Task
+    {
+
+        [Key]
+        public int TaskId { get; set; }
+
+        [Required]
+        public string TaskName { get; set; }
+
+        public enum TaskStatus
+        {
+            ToDo,
+            InProgress,
+            Done
+        }
+
+        // Maximum and minimum task description length 
+        [StringLength(1000, MinimumLength = 100)]
+        [Column(TypeName = "varchar(1000)")]
+        public string? TaskDescription { get; set; }
+
+        [Required]
+        public int TaskPriority { get; set; }
+    }
+}
