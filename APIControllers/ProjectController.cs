f13920a2eb8366063ade26f68914075a518c5bf7
MilanVulin <vulinmb@gmail.com>
2022-04-16 20:44:51 +0200

    Implemented the ProjectController class

--- APIControllers/ProjectController.cs
+++ APIControllers/ProjectController.cs
@@ -1,6 +1,51 @@
-﻿namespace Akvelon_Internship_Test_Task.APIControllers
+﻿using Akvelon_Internship_Test_Task.Models;
+using Akvelon_Internship_Test_Task.Repositories;
+using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+
+namespace Akvelon_Internship_Test_Task.APIControllers
 {
-    public class ProjectController
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ProjectController : ControllerBase
     {
+        private readonly IGenericRepository<Project> _repo;
+
+        public ProjectController(IGenericRepository<Project> repo)
+        {
+            _repo = repo;
+        }
+
+        [HttpGet]
+        public IEnumerable<Project> GetAllProjects()
+        {
+            return _repo.GetAll();
+        }
+
+        [HttpGet]
+        [Route("{id}")]
+        public Project GetProjectById(int id)
+        {
+            return _repo.GetById(id);
+        }
+
+        [HttpPost]
+        public void AddProject(Project proj)
+        {
+            _repo.Add(proj);
+        }
+
+        [HttpDelete]
+        public void RemoveProject(Project proj)
+        {
+            _repo.Remove(proj);
+        }
+
+        [HttpPut]
+        public void UpdateProject(Project proj)
+        {
+            _repo.Update(proj);
+        }
+
     }
 }
