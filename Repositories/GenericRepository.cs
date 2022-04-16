ca114e485d29421c4677e172f0c4aab47831ac57
MilanVulin <vulinmb@gmail.com>
2022-04-16 19:49:31 +0200

    Implemented the repository interface and class

--- Repositories/GenericRepository.cs
+++ Repositories/GenericRepository.cs
@@ -1,6 +1,64 @@
-﻿namespace Akvelon_Internship_Test_Task.Repositories
+﻿using Akvelon_Internship_Test_Task.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+
+namespace Akvelon_Internship_Test_Task.Repositories
 {
     public class GenericRepository<T> : IGenericRepository<T> where T : class
     {
+
+        // Get the context of our App/DB
+        protected readonly AppDbContext _context;
+
+        // Create a public constructor
+        public GenericRepository(AppDbContext context)
+        {
+            _context = context;
+        }
+
+        // Implementation of all of the interface methods (CRUD Operations)
+        public IEnumerable<T> GetAll()
+        {
+            return _context.Set<T>().ToList();
+        }
+
+        public T GetById(int id)
+        {
+            return _context.Set<T>().Find(id);
+        }
+
+        // Returns a list of specified type for all the entities int he DB that match the parameter (predicate) expression
+        public IEnumerable<T> Find(Expression<Func<T, bool>> expression)
+        {
+            return _context.Set<T>().Where(expression);
+        }
+
+        public void Add(T entity)
+        {
+            _context.Set<T>().Add(entity);
+        }
+
+        public void AddRange(IEnumerable<T> entities)
+        {
+            _context.Set<T>().AddRange(entities);
+        }
+
+        public void Remove(T entity)
+        {
+            _context.Set<T>().Remove(entity);
+        }
+
+        public void RemoveRange(IEnumerable<T> entities)
+        {
+            _context.Set<T>().RemoveRange(entities);
+        }
+
+        public void Update(T entity)
+        {
+            _context.Set<T>().Update(entity);
+        }
+
     }
 }
--- Repositories/IGenericRepository.cs
+++ Repositories/IGenericRepository.cs
@@ -1,9 +1,25 @@
-﻿namespace Akvelon_Internship_Test_Task.Repositories
+﻿using System;
+using System.Collections.Generic;
+using System.Linq.Expressions;
+
+namespace Akvelon_Internship_Test_Task.Repositories
 {
     public interface IGenericRepository<T>
     {
         // Repository Pattern Implementation 
         // This is our generic repository for data access layer,
         // it will be inherited and implemented by our repository class
+        public interface IGenericRepository<T> where T : class
+        {
+            T GetById(int id);
+            IEnumerable<T> GetAll();
+            IEnumerable<T> Find(Expression<Func<T, bool>> expression);
+            void Add(T entity);
+            void AddRange(IEnumerable<T> entities);
+            void Remove(T entity);
+            void RemoveRange(IEnumerable<T> entities);
+            void Update(T entity);
+        }
+
     }
 }
