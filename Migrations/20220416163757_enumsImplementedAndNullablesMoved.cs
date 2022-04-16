f6461cfd48d882ef399c8b157063bd015b39723d
MilanVulin <vulinmb@gmail.com>
2022-04-16 18:40:36 +0200

    Updated the Db via migrations to match the current models

---
+++ Migrations/20220416163757_enumsImplementedAndNullablesMoved.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Akvelon_Internship_Test_Task.Migrations
+{
+    public partial class enumsImplementedAndNullablesMoved : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "Status",
+                table: "Task",
+                type: "int",
+                nullable: false,
+                defaultValue: 0);
+
+            migrationBuilder.AddColumn<int>(
+                name: "Status",
+                table: "Project",
+                type: "int",
+                nullable: false,
+                defaultValue: 0);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Status",
+                table: "Task");
+
+            migrationBuilder.DropColumn(
+                name: "Status",
+                table: "Project");
+        }
+    }
+}
