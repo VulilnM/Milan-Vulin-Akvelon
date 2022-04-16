f0d8a5bebf91f06187602d9d46c03d4f4348ac1a
MilanVulin <vulinmb@gmail.com>
2022-04-16 20:51:42 +0200

    Implemented TaskController class

--- APIControllers/TaskController.cs
+++ APIControllers/TaskController.cs
@@ -1,6 +1,53 @@
-﻿namespace Akvelon_Internship_Test_Task.APIControllers
+﻿using Akvelon_Internship_Test_Task.Models;
+using Akvelon_Internship_Test_Task.Repositories;
+using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+
+
+namespace TestTaskProjAkv.Controllers
 {
-    public class TaskController
+
+    [Route("api/[controller]")]
+    [ApiController]
+    public class TaskController : ControllerBase
     {
+        private readonly IGenericRepository<Task> _repo;
+
+        public TaskController(IGenericRepository<Task> repo)
+        {
+            _repo = repo;
+        }
+
+        [HttpGet]
+        public IEnumerable<Task> GetAllTasks()
+        {
+            return _repo.GetAll();
+        }
+
+        [HttpGet]
+        [Route("{id}")]
+        public Task GetTaskById(int id)
+        {
+            return _repo.GetById(id);
+        }
+
+        [HttpPost]
+        public void AddTask(Task task)
+        {
+            _repo.Add(task);
+        }
+
+        [HttpDelete]
+        public void RemoveTask(Task task)
+        {
+            _repo.Remove(task);
+        }
+
+        [HttpPut]
+        public void UpdateProject(Task task)
+        {
+            _repo.Update(task);
+        }
+
     }
 }
