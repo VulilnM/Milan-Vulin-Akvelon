8e8baa9df72d60c463b629cd12388e133c13a3fb
MilanVulin <vulinmb@gmail.com>
2022-04-16 19:46:12 +0200

    Added empty Repository for communication with the controllers

---
+++ Repositories/GenericRepository.cs
@@ -0,0 +1,6 @@
+﻿namespace Akvelon_Internship_Test_Task.Repositories
+{
+    public class GenericRepository<T> : IGenericRepository<T> where T : class
+    {
+    }
+}
---
+++ Repositories/IGenericRepository.cs
@@ -0,0 +1,9 @@
+﻿namespace Akvelon_Internship_Test_Task.Repositories
+{
+    public interface IGenericRepository<T>
+    {
+        // Repository Pattern Implementation 
+        // This is our generic repository for data access layer,
+        // it will be inherited and implemented by our repository class
+    }
+}
