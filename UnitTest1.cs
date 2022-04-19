e0dbf1b65b362a1a45e095a6261b4467a5e3afa9
MilanVulin <vulinmb@gmail.com>
2022-04-19 12:51:52 +0200

    Add project files.

---
+++ UnitTest1.cs
@@ -0,0 +1,14 @@
+using System;
+using Xunit;
+
+namespace Akvelon_Internship_Test_Task_xUnit
+{
+    public class UnitTest1
+    {
+        [Fact]
+        public void Test1()
+        {
+
+        }
+    }
+}
